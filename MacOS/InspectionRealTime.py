58bded5d2afe287f96050188022bd9b999ddff2e
hanjiung <smert2580@naver.com>
2021-04-04 20:26:58 +0900

    revise file structure

---
+++ MacOS/InspectionRealTime.py
@@ -0,0 +1,27 @@
+import psutil
+import polling2
+import time
+
+def CheckDocument():
+
+    for proc in psutil.process_iter():
+        
+        if proc.name() == "Microsoft Excel":
+            time.sleep(1)
+            path = []
+            for i in proc.open_files():
+                if i[0][-4:] == "xlsx":
+                    path.append(i[0])
+
+                    
+            proc.kill()
+            
+            
+            if len(path) != 0:
+                for i in path:
+                    if i.find("$") == -1:
+                        return i
+
+            else:
+                break
+
